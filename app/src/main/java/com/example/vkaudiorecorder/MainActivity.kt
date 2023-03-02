8ca2db6fcfe6f7396bb20c4b4900d4ff7634b488
Daniil <favdela4tune@gmail.com>
2023-03-02 17:30:18 +0300

    migration from Material to Material3

--- app/src/main/java/com/example/vkaudiorecorder/MainActivity.kt
+++ app/src/main/java/com/example/vkaudiorecorder/MainActivity.kt
@@ -7,20 +7,24 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Mic
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
 import com.example.vkaudiorecorder.ui.MOCKED_RECORD_LIST
 import com.example.vkaudiorecorder.ui.model.Record
 import com.example.vkaudiorecorder.ui.theme.VKAudioRecorderTheme
@@ -33,7 +37,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.primary
                 ) {
                     MainScreen()
                 }
@@ -44,24 +48,77 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    Column {
-        Text(text = "Ваши записи")
+    Column(horizontalAlignment = CenterHorizontally) {
+        Text(
+            text = "Ваши записи", modifier = Modifier
+            .align(Start)
+            .padding(start = 16.dp, top = 8.dp, bottom = 24.dp),
+            style = MaterialTheme.typography.labelLarge
+        )
         LazyColumn(
             modifier = Modifier.align(CenterHorizontally),
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 12.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             items(MOCKED_RECORD_LIST) { record ->
                 RecordItem(record)
             }
         }
+        Spacer(modifier = Modifier.weight(1f))
+        RecordButton()
+    }
+}
+
+@Composable
+fun RecordButton() {
+    Surface(
+        modifier = Modifier
+            .padding(36.dp)
+            .size(60.dp)
+            .shadow(8.dp, shape = CircleShape),
+        shape = CircleShape,
+        color = MaterialTheme.colorScheme.primary,
+    ) {
+        Icon(imageVector = Icons.Filled.Mic, contentDescription = null, Modifier.fillMaxSize(0.5f))
     }
 }
 
 @Composable
 fun RecordItem(record: Record) {
     Card(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(4.dp),
+        shape = RoundedCornerShape(12.dp)
     ) {
-        ConstraintLayout {
+        Row {
+            Column(modifier = Modifier.weight(0.4f)) {
+                Text(
+                    text = record.title,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.padding(top = 8.dp, start = 8.dp),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                Text(
+                    text = record.dateToString(),
+                    modifier = Modifier.padding(start = 8.dp, bottom = 8.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+            Text(
+                text = record.durationToString(),
+                modifier = Modifier
+                    .align(CenterVertically)
+                    .padding(8.dp),
+                style = MaterialTheme.typography.bodyMedium
+            )
+            PlayButton(modifier = Modifier
+                .size(32.dp)
+                .align(CenterVertically)
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+        }
+       /* ConstraintLayout {
             val (title, date, duration, playButton) = createRefs()
 
             Text(modifier = Modifier.constrainAs(title) {
@@ -81,28 +138,33 @@ fun RecordItem(record: Record) {
                 bottom.linkTo(parent.bottom)
                 top.linkTo(parent.top)
             }, text = record.durationToString())
-            PlayButton(modifier = Modifier.constrainAs(playButton) {
-                end.linkTo(parent.end, margin = 8.dp)
-                start.linkTo(duration.end, margin = 4.dp)
-                top.linkTo(parent.top)
-                bottom.linkTo(parent.bottom)
-            }.size(48.dp))
+            PlayButton(modifier = Modifier
+                .constrainAs(playButton) {
+                    end.linkTo(parent.end, margin = 8.dp)
+                    start.linkTo(duration.end, margin = 4.dp)
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                }
+                .size(36.dp))
         }
-
+*/
 
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PlayButton(modifier: Modifier) {
     val isPlaying = remember { mutableStateOf(false) }
-    Button(
+    Surface(
         modifier = modifier,
         shape = CircleShape,
+        color = MaterialTheme.colorScheme.primary,
         onClick = { isPlaying.value = !isPlaying.value }) {
         Icon(
             imageVector = if (isPlaying.value) Icons.Filled.Pause else Icons.Filled.PlayArrow,
-            contentDescription = null
+            contentDescription = null,
         )
     }
 }
--- app/src/main/java/com/example/vkaudiorecorder/ui/theme/Type.kt
+++ app/src/main/java/com/example/vkaudiorecorder/ui/theme/Type.kt
@@ -1,6 +1,6 @@
 package com.example.vkaudiorecorder.ui.theme
 
-import androidx.compose.material.Typography
+import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -8,11 +8,36 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+    ),
+    labelLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+    ),
+    labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
+        fontSize = 12.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Thin,
+        fontSize = 24.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Light,
+        fontSize = 16.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Thin,
+        fontSize = 12.sp,
+    ),
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
